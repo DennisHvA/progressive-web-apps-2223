242ec68d07feaaf0035e1df7112b4374cab4620c
DennisHvA <dennis.de.graaf2@hva.nl>
2023-03-21 13:18:28 +0100

    api

--- excercises/wafs/router/index.js
+++ excercises/wafs/router/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const request = require('request');
 const router = express.Router();
+const fetch = require('node-fetch')
 
 router.get('/', (req, res) => {
     res.render('home')
+    fetch(`https://world.openfoodfacts.org/api/v0/product/5449000000996.json`)
+    .then(async response => {
+      const movieData = await response.json()
+      console.log(movieData)
+    })
 })
 
 router.get('/:id', (req, res) => {
