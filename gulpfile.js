cd63f0a012c450470c8cbe71bbb95e9a52495ac0
DennisHvA <dennis.de.graaf2@hva.nl>
2023-04-04 17:35:30 +0200

    gulp sw

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const minify = require('gulp-minify');
+const uglify = require('gulp-uglify');
+
 
 gulp.task('cleanCSS', () => {
     return gulp.src('src/styles/*.css')
@@ -8,10 +10,16 @@ gulp.task('cleanCSS', () => {
       .pipe(gulp.dest('./public/styles'));
 });
 
+// gulp.task('compressSW', function() {
+//     gulp.src(['src/service-worker.js'])
+//       .pipe(minify())
+//       .pipe(gulp.dest('./public'))
+// });
+
 gulp.task('compressSW', function() {
-    gulp.src(['./public/service-worker.js'])
-      .pipe(minify())
-      .pipe(gulp.dest('./public'))
+  return gulp.src('src/service-worker.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/'))
 });
 
 gulp.task('default', gulp.series(['cleanCSS', 'compressSW']))
